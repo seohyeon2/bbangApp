06a3454e117ab37ebe99750866ef0b2bd3481d5a
seohyeon2 <seo_hyeon418@naver.com>
2023-02-02 01:32:46 +0900

    init: 프로젝트 생성

---
+++ BBANG/BBANG/BBANGApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBANGApp.swift
+//  BBANG
+//
+//  Created by seohyeon park on 2023/02/02.
+//
+
+import SwiftUI
+
+@main
+struct BBANGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
