43c204d6635c468e8077c3ae8b75b5d78182c3b8
seohyeon2 <seo_hyeon418@naver.com>
2023-02-02 19:59:22 +0900

    feat: Home 뷰 - 추천탭 - 남은 뷰 구현
    - 라인 뷰 (Line)
    - 가로스크롤 가능 상품 뷰 (ProductList)
    - 추천 브랜드확인 뷰 (RecommendedBrandList)
    - 광고 뷰 (Advertising)
    - 앱 정보 뷰 (Info)

--- BBANG/BBANG/View/Home/Recommendation.swift
+++ BBANG/BBANG/View/Home/Recommendation.swift
@@ -13,6 +13,13 @@ struct Recommendation: View {
             VStack {
                 Banner()
                 CircleMenu()
+                Line()
+                ProductList()
+                Line()
+                RecommendedBrandList()
+                Advertising(imageName: "banner1")
+                ProductList()
+                Info()
             }
         }
     }
@@ -204,5 +211,177 @@ struct CircleMenu: View {
             }
             .padding([.leading, .trailing], 20)
         }
+    }
+}
+
+struct Line: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.gray)
+            .frame(height: 1)
+            .opacity(0.2)
+            .padding()
+    }
+}
+
+struct ProductList: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("New Product")
+                        .foregroundColor(.black)
+                        .font(.system(.headline))
+                    Text("이달의 신상 빵")
+                        .foregroundColor(.gray)
+                        .font(.system(.footnote))
+                }
+                Spacer()
+            }
+            
+            ScrollView(.horizontal) {
+                VStack(alignment: .leading) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.gray)
+                            .opacity(0.2)
+                            .frame(width: 150, height: 150)
+                        // TODO: 신상 빵 데이터 추가하기 (사진, 빵이름, 브랜드이름, 가격)
+                        Image("circleButtonEvent")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100, height: 100)
+                            .overlay (
+                                Button(action: {
+                                    
+                                }, label: {
+                                    Image(systemName: "bookmark")
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 15, height: 3)
+                                        .foregroundColor(.black)
+                                })
+                                .padding(.leading, 110)
+                                .padding(.top, 100)
+                            )
+                    }
+                    
+                    Text("도넛1")
+                        .foregroundColor(.black)
+                        .font(.system(.footnote))
+                        .bold()
+                    Text("던킨도넛츠")
+                        .foregroundColor(.black)
+                        .font(.system(.footnote))
+                    Text("1,000원")
+                        .foregroundColor(.black)
+                        .font(.system(.footnote))
+                        .bold()
+                        .padding(.top, 2)
+                }
+            }
+        }
+    }
+}
+
+// TODO: 브랜드 데이터 추가하기
+struct RecommendedBrandList: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Recommended Brand")
+                        .foregroundColor(.black)
+                        .font(.system(.headline))
+                    Text("추천 브랜드")
+                        .foregroundColor(.gray)
+                        .font(.system(.footnote))
+                }
+                Spacer()
+            }
+            ScrollView(.horizontal) {
+                HStack {
+                    VStack {
+                        Button {
+                            
+                        } label: {
+                            Image("circleButtonPoint")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 35, height: 35)
+                        }
+                        .frame(width: 40, height: 40)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                        
+                        Text("브랜드1")
+                            .foregroundColor(.black)
+                            .font(.system(.footnote))
+                    }
+                    .padding(.top, 10)
+                    .padding(.trailing, 30)
+                    
+                    VStack {
+                        Button {
+                            
+                        } label: {
+                            Image("circleButtonPoint")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 35, height: 35)
+                        }
+                        .frame(width: 40, height: 40)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                        
+                        Text("브랜드2")
+                            .foregroundColor(.black)
+                            .font(.system(.footnote))
+                    }
+                    .padding(.top, 10)
+                    .padding(.trailing, 30)
+                    
+                    VStack {
+                        Button {
+                            
+                        } label: {
+                            Image("circleButtonPoint")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 35, height: 35)
+                        }
+                        .frame(width: 40, height: 40)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                        
+                        Text("브랜드3")
+                            .foregroundColor(.black)
+                            .font(.system(.footnote))
+                    }
+                    .padding(.top, 10)
+                    .padding(.trailing, 30)
+                }
+            }
+        }
+    }
+}
+
+struct Advertising: View {
+    var imageName: String
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(height: 150)
+            .padding([.top, .bottom], 80)
+    }
+}
+
+struct Info: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.green)
+            .frame(height: 500)
+            .padding(.top, 20)
     }
 }
